aad6a11dccdbbfe4e473ce1bfbf6b0f6cd26457f
Corey Light <coreylight@gmail.com>
2023-09-28 14:46:10 -0500

    last rust commit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,50 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::{Blob, File, FileReader, Url};
+use web_sys::{Blob, Url};
+use web_sys::HtmlElement;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::collections::HashMap;
 use js_sys;
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Response {
+    status: usize,
+    status_text: String,
+    http_version: String,
+    headers: HashMap<String, String>,
+    cookies: Vec<Cookie>,
+    content: Content,
+    redirect_url: String,
+    headers_size: usize,
+    body_size: usize,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Cookie {
+    name: String,
+    value: String,
+    path: Option<String>,
+    domain: Option<String>,
+    expires: Option<String>,
+    http_only: Option<bool>,
+    secure: Option<bool>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Content {
+    size: usize,
+    mime_type: String,
+    text: Option<String>,
+    encoding: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Creator {
+    name: String,
+    version: String,
+}
+
 
 #[derive(Deserialize, Serialize)]
 struct Har {
@@ -65,38 +105,30 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn process_file(file: File) {
-    let reader = FileReader::new().unwrap();
-    let onload = Closure::wrap(Box::new(move |event: web_sys::Event| {
-        let reader = event.target().unwrap().dyn_into::<FileReader>().unwrap();
-        let contents = reader.result().unwrap().as_string().unwrap();
+pub fn process_file(contents: &str) {
+    let mut har: Har = serde_json::from_str(&contents).unwrap_or_else(|err| {
+        log(&format!("Error parsing HAR file: {:?}", err));
+        Har::default()
+    });
 
-        // Parse the HAR file
-        let mut har: Har = serde_json::from_str(&contents).unwrap_or_else(|err| {
-            log(&format!("Error parsing HAR file: {:?}", err));
-            Har::default()
-        });
+    // Strip out the Authorization headers
+    strip_auth_headers(&mut har);
 
-        // Strip out the Authorization headers
-        strip_auth_headers(&mut har);
+    // Serialize back to JSON
+    let output = serde_json::to_string_pretty(&har).unwrap();
+    let output_js_value = JsValue::from_str(&output);
+    let blob = Blob::new_with_str_sequence(&js_sys::Array::of1(&output_js_value)).unwrap();
+    let url = Url::create_object_url_with_blob(&blob).unwrap();
 
+    // Trigger download
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let anchor = document.create_element("a").unwrap().dyn_into::<HtmlElement>().unwrap();
+    anchor.set_attribute("href", &url).unwrap();
+    anchor.set_attribute("download", "har.json").unwrap();
+    anchor.click();
 
-
-        // Serialize back to JSON
-        let output = serde_json::to_string_pretty(&har).unwrap();
-        let output_js_value = JsValue::from_str(&output);
-        let blob = Blob::new_with_str_sequence(&js_sys::Array::of1(&output_js_value)).unwrap();
-        let url = Url::create_object_url_with_blob(&blob).unwrap();
-
-        // Trigger download
-        web_sys::window().unwrap().open_with_url_and_target(&url, "_blank").unwrap();
-
-        Url::revoke_object_url(&url).unwrap();
-    }) as Box<dyn FnMut(_)>);
-
-    reader.set_onload(Some(onload.as_ref().unchecked_ref()));
-    reader.read_as_text(&file).unwrap();
-    onload.forget();
+    Url::revoke_object_url(&url).unwrap();
 }
 
 fn main() {}
